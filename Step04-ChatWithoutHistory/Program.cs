f2ef14b2db7e506eb4ca4bc8cbc4624cf3ad30c4
Adrien Clerbois <adrien.clerbois@outlook.com>
2024-11-19 08:43:55 +0100

    Add Steps

---
+++ Step04-ChatWithoutHistory/Program.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.SemanticKernel.ChatCompletion;
+using Microsoft.SemanticKernel.Connectors.AzureOpenAI;
+using Microsoft.SemanticKernel.Connectors.OpenAI;
+
+Console.WriteLine("Hello AI World");
+Console.WriteLine("===============");
+
+//IChatCompletionService chatService = new AzureOpenAIChatCompletionService("gpt-4o", "https://devday-semantickernel-test.openai.azure.com/", Environment.GetEnvironmentVariable("AI:AzureOpenAI:Key")); 
+IChatCompletionService chatService = new OpenAIChatCompletionService("gpt-3.5-turbo-0125", Environment.GetEnvironmentVariable("AI:OpenAI:Key")); Console.WriteLine("Configuration : OpenAI - gpt-4o-mini");
+
+while (true)
+{
+    Console.Write("Question ? : ");
+    Console.WriteLine(await chatService.GetChatMessageContentAsync(Console.ReadLine()));
+}
---
+++ Step06-AddPersona/Program.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.SemanticKernel.ChatCompletion;
+using Microsoft.SemanticKernel.Connectors.AzureOpenAI;
+
+Console.WriteLine("Hello AI World");
+Console.WriteLine("===============");
+
+IChatCompletionService chatService = new AzureOpenAIChatCompletionService("gpt-4o", "https://devday-semantickernel-test.openai.azure.com/", Environment.GetEnvironmentVariable("AI:AzureOpenAI:Key")); 
+
+ChatHistory history = new();
+history.AddAssistantMessage("Bonjour, tu es une IA râleuse, et tu t'appels Romain.");
+
+while (true)
+{
+    Console.Write("Question ? : ");
+    history.AddUserMessage(Console.ReadLine());
+
+    var assistant = await chatService.GetChatMessageContentAsync(history);
+    history.Add(assistant);
+    Console.WriteLine(assistant);
+}
